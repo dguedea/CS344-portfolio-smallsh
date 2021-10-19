344f903dc64d656fe09dae52bcdb5e8209aeb46f
dguedea <guedead@oregonstate.edu>
2021-10-19 11:43:19 +0200

    added parsing user input to struct

--- main.c
+++ main.c
@@ -29,22 +29,34 @@
 
 /************************************************************************************************
  * STRUCT FOR USER INPUT
+ * Will capture command, a linked list of arguments, the inputFile (if any),
+ * the outputFile (if any) and if it should run in the background
  ************************************************************************************************/
 
+// Struct to store user input to create the full command
 struct input
 {
     char* command;
-    char arg[MAX_ARG];
+    // struct arguments *listArgs;
+    char listArgs[MAX_ARG];
     char* inputFile;
     char* outputFile;
-    bool isBackground;
+    int isBackground;
 };
+
+// To create a linked list of arguments since there can be multiple
+// struct arguments 
+// {
+//     char *arg;
+//     struct arguments *next;
+// };
 
 /************************************************************************************************
  * INITIALIZING FUNCTIONS USED IN MAIN
  ************************************************************************************************/
 void getInput(char *input);
-void parseInput();
+void printCommand(struct input *aCommand);
+struct input *parseUserInput(char *input);
 
 
 /*************************************************************************************************
@@ -70,8 +82,14 @@ int main() {
     // Start loop that will make up the smallsh getting user input and executing
     //while (*smallLoop == 0) {
 
-    // Get user input and parse
+    // Get user input
     getInput(userInput);
+
+    // Parse user input into input struct
+    parseUserInput(userInput);
+
+    // Understand command and redirect
+    
 
 
 
@@ -104,6 +122,26 @@ void getInput(char *input) {
     fflush(stdout);
     fgets(input, MAX_COMMAND, stdin); 
 
+    // remove new line from end of input
+    // must do this or else the strtok_r does not notice the & at end
+    input[strcspn(input, "\n")] = 0;
+
+    // // Return back to loop if comment
+    // if(input[0] == '#'){
+    //     printf("Comment");
+    //     return;
+    // }
+    // // Return back to loop if new line (just enter from user)
+    // else if(strcmp(input, "\n")==0){
+    //     printf("New Line");
+    //     return;
+    // }
+    // // Parse
+    // else {
+    //     parseUserInput(input);
+    // }
+    return;
+
 }
 
 /************************************************************************************************
@@ -113,5 +151,71 @@ void getInput(char *input) {
  ************************************************************************************************/
 
 struct input *parseUserInput(char *input) {
-    
+
+    // Initialize the struct with isBackground initialized to false
+    struct input *newCmd = malloc(sizeof(struct input));
+    newCmd->command = NULL;
+    newCmd->inputFile = NULL;
+    newCmd->outputFile = NULL;
+    newCmd->isBackground = 0;
+
+    // Set up linked list for argument
+    // struct arguments *newArg = malloc(sizeof(struct arguments));
+    // struct arguments *head = NULL;
+    // struct arguments *tail = NULL;
+
+    char *saveptr;
+
+    // Store command since that is always first
+    char *token = strtok_r(input, " ", &saveptr);
+    newCmd->command = calloc(strlen(token) + 1, sizeof(char));
+    strcpy(newCmd->command, token);
+
+    // Loop through rest to assign to struct
+    while((token = strtok_r(NULL, " ", &saveptr))) {
+        // check if input file
+        if(strcmp(token, "<") == 0) {
+            token = strtok_r(NULL, " ", &saveptr);
+            newCmd->inputFile = calloc(strlen(token)+1, sizeof(char));
+            strcpy(newCmd->inputFile, token);
+        }
+        // check if output file
+        else if(strcmp(token, ">") == 0) {
+            token = strtok_r(NULL, " ", &saveptr);
+            newCmd->outputFile = calloc(strlen(token)+1, sizeof(char));
+            strcpy(newCmd->outputFile, token);
+        }
+        // check if background process
+        else if(strcmp(token, "&") == 0) {
+            newCmd->isBackground = 1;
+        }
+        // else add to argument char array with ; in between
+        else {
+            strcat(newCmd->listArgs, token);
+            strcat(newCmd->listArgs, ";");
+        }
+    }
+    fflush(stdout);
+
+    printCommand(newCmd);
+    return newCmd;
+
 }
+
+/******************************************************************************
+ * printCommand
+ * Descripton: Print data from command struct (used in testing file parsing)
+ * 
+ * Adapted from students.c from Assignment 1 344
+ * CS 344 (Sept. 13, 2020) Citing source code https://replit.com/@cs344/studentsc#student_info1.txt
+ * ****************************************************************************/
+
+void printCommand(struct input *aCommand)
+{
+    printf("Command: %s\n Input: %s\n Output: %s\n Args: %s\n Background: %d\n", aCommand->command,
+           aCommand->inputFile,
+           aCommand->outputFile,
+           aCommand->listArgs,
+           aCommand->isBackground);
+}
+
